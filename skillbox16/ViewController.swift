8314d572d1a638ed8093e0c2fdd8143f9fce6b32
Никитин Дмитрий <d.nikitin@mt-innovations.ru>
2022-12-04 14:36:09 +0300

    commit1

---
+++ skillbox16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  skillbox16
+//
+//  Created by Nikitin Dmitriy on 04.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
